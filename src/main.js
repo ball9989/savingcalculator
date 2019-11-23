082b81c99742c8f0f67d4ff8d6c85669e2d5b84b
ballballball <62070006@it.kmitl.ac.th>
2019-11-23 13:01:45 +0700

    Connet to Firebase

--- src/main.js
+++ src/main.js
@@ -3,8 +3,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import * as firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBuE8lWWdYTPegFBwetoCmqqeOmnojMZDM",
+  authDomain: "savingcalculator-3f83a.firebaseapp.com",
+  databaseURL: "https://savingcalculator-3f83a.firebaseio.com",
+  projectId: "savingcalculator-3f83a",
+  storageBucket: "savingcalculator-3f83a.appspot.com",
+  messagingSenderId: "7397992000",
+  appId: "1:7397992000:web:ba63766a807075a99de1a5",
+  measurementId: "G-EZBBZ14D1D"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 /* eslint-disable no-new */
 new Vue({
