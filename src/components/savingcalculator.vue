78ee3a731db60188e46cc9a1776f87f3c8ccaf1f
ballballball <62070006@it.kmitl.ac.th>
2019-11-26 09:15:09 +0700

    สร้างการคำนวณ

--- src/components/savingcalculator.vue
+++ src/components/savingcalculator.vue
@@ -9,48 +9,25 @@
       <form v-on:submit.prevent="sendForm()">
         <div class="row mt-5">
           <div class="col-12">
-            <div class="form-row">
-              <div class="col">
-                <label for="dato">วันที่ :</label>
-                <input
-                  type="date"
-                  id="dato"
-                  class="form-control"
-                  v-model="form.date"
-                  placeholder="วันที่"
-                  required
-                />
-              </div>
-              <div class="col">
-                <label for="incoma">รายรับ (บาท) :</label>
-                <input
-                  type="number"
-                  min="0"
-                  max="1000000"
-                  id="incoma"
-                  class="form-control"
-                  v-model="form.income"
-                  placeholder="รายรับ"
-                  required
-                />
-              </div>
-              <div class="col">
-                <label for="raijai">รายจ่าย (บาท) :</label>
-                <input
-                  type="number"
-                  id="raijai"
-                  class="form-control"
-                  v-model="form.expense"
-                  placeholder="รายจ่าย"
-                  required
-                />
-              </div>
-            </div>
-          </div>
+                    <div class="form-row">
+                        <div class="col">
+                            <label for="dato">วันที่ :</label>
+                            <input type="date" id="dato" class="form-control" v-model="form.date" placeholder="วันที่" required>
+                        </div>
+                        <div class="col">
+                            <label for="incoma">รายรับ (บาท) :</label>
+                            <input type="number" min="0" max="1000000" id="incoma" class="form-control" v-model="form.income" placeholder="รายรับ" required>
+                        </div>
+                        <div class="col">
+                            <label for="raijai">รายจ่าย (บาท) :</label>
+                            <input type="number" id="raijai" class="form-control" v-model="form.expense" placeholder="รายจ่าย" required>
+                        </div>
+                    </div>
+                </div>
         </div>
         <div class="row mt-5">
           <div class="col-12 text-center">
-            <button type="submit" class="btn btn-success btn-lg">ส่ง</button>
+            <button class="btn btn-success btn-lg" type="submit">ส่ง</button>
             <button type="button" class="btn btn-primary btn-lg" @click="removeAllRows()">ล้างรายการ</button>
             <button
               class="btn btn-danger btn-lg"
@@ -71,8 +48,9 @@
                 <div class="col-3">
                     <label for="dato">เงินเก็บเฉลี่ยต่อวัน</label>
                 </div>
-          </div>
-          <div class="row bg-light text-center pt-2 pb-2 mb-5">
+            </div>
+        <div v-if="this.transactionList.length">
+            <div class="row bg-light text-center pt-2 pb-2 mb-5">
                 <div class="col-3">
                     {{incomeSum}} บาท   
                 </div>
@@ -86,6 +64,12 @@
                     {{balanceAvg}} บาท
                 </div>
             </div>
+        </div>
+        <div v-else class="row pb-2 pt-2 border-dark bg-light text-center"> 
+                  <div class="col-12">
+                      ไม่มีรายการให้แสดง
+                  </div>
+        </div>
         <div class="row bg-dark text-center">
           <div class="col-3">
             <label for="dato">วันที่</label>
@@ -104,20 +88,20 @@
           </div>
         </div>
         <div v-if="this.transactionList.length">
-          <div v-for="row in transactionList">
-            <div class="row pb-2 pt-2 border-bottom border-dark bg-light" style="text-align:center">
-              <div class="col-3">{{row.data.tranData.date}}</div>
-              <div class="col-2">{{row.data.tranData.income}} บาท</div>
-              <div class="col-2">{{row.data.tranData.expense}} บาท</div>
-              <div class="col-3">{{row.data.balance}} บาท</div>
-              <div class="col-2">
-                <button type="button" class="btn btn-danger btn-sm" @click="removeRow(row.id)">ลบแถว</button>
-              </div>
-            </div>
+                  <div v-for="row in transactionList">
+                      <div class="row pb-2 pt-2 border-bottom border-dark bg-light" style="text-align:center">
+                          <div class="col-3">{{row.data.tranData.date}}</div>
+                          <div class="col-2">{{row.data.tranData.income}} บาท</div>
+                          <div class="col-2">{{row.data.tranData.expense}} บาท</div>
+                          <div class="col-3">{{row.data.balance}} บาท</div>
+                          <div class="col-2"><button type="button"class="btn btn-danger btn-sm" @click="removeRow(row.id)">ลบแถว</button></div>
+                      </div>
+                  </div>
           </div>
-        </div>
-        <div v-else class="row pb-2 pt-2 border-dark bg-light text-center">
-          <div class="col-12">ไม่มีรายการให้แสดง</div>
+        <div v-else class="row pb-2 pt-2 border-dark bg-light text-center"> 
+                  <div class="col-12">
+                      ไม่มีรายการให้แสดง
+                  </div>
         </div>
       </form>
     </div>
@@ -125,22 +109,26 @@
 </template>
 
 <script>
-import { firestore } from "firebase";
+import {firestore} from 'firebase'
 export default {
-  mounted() {
-    this.getData();
-  },
-  data() {
-    return {
-      form: {
-        date: "",
-        income: 0,
-        expense: 0
-      },
-      transactionList: [],
-      username: this.$route.params.username
-    };
-  },
+  mounted () {
+        this.getData ()
+    },
+  data () {
+        return {
+            form: {
+                date: '',
+                income: 0,
+                expense: 0,
+            },
+            incomeSum: 0,
+            expenseSum: 0,
+            goal: 0,
+            totalBalance: 0,
+            transactionList: [],
+            username: this.$route.params.username
+        }
+    },
   methods: {
     sendForm() {
       var timestamp = this.getTimeStamp();
@@ -158,48 +146,68 @@ export default {
           this.getData();
         });
     },
-    getData() {
-      firestore()
-        .collection("transactions")
-        .where("owner", "==", this.username)
-        .orderBy("tranData.date", "asc")
-        .orderBy("timeStamp", "asc")
-        .onSnapshot(snapshot => {
-          // console.log(snapshot)
-          var newList = [];
-          this.incomeSum = 0;
-          this.expenseSum = 0;
-          this.totalBalance = 0;
-          snapshot.forEach(doc => {
-            newList.push({ id: doc.id, data: doc.data() });
-            // console.log(doc.data())
-            this.incomeSum += parseInt(doc.data().tranData.income);
-            this.expenseSum += parseInt(doc.data().tranData.expense);
-            this.totalBalance += parseInt(doc.data().balance);
-          });
-          this.transactionList = newList;
-        });
-    },
-    getTimeStamp() {
-      return new Date().getTime();
-    },
+    
+    getData () {
+            firestore()
+            .collection('transactions')
+            .where('owner', '==', this.username)
+            .orderBy('tranData.date', 'asc')
+            .orderBy('timeStamp', 'asc')
+            .onSnapshot(snapshot => {
+                // console.log(snapshot)
+                var newList = []
+                this.incomeSum = 0
+                this.expenseSum = 0
+                this.totalBalance = 0
+                snapshot.forEach(doc => {
+                    newList.push({id: doc.id, data: doc.data()})
+                    // console.log(doc.data())
+                    this.incomeSum += parseInt(doc.data().tranData.income)
+                    this.expenseSum += parseInt(doc.data().tranData.expense)
+                    this.totalBalance += parseInt(doc.data().balance)
+                });
+                this.transactionList = newList
+            })
+        },
     removeRow(e) {
-      firestore()
-        .collection("transactions")
-        .doc(e)
-        .delete();
-    },
+            firestore()
+            .collection('transactions')
+            .doc(e)
+            .delete()
+        },
     removeAllRows() {
-      firestore()
-        .collection("transactions")
-        .get()
-        .then(snapshot => {
-          snapshot.forEach(doc => {
-            if (doc.data().owner == this.username) this.removeRow(doc.id);
-          });
-        });
+            firestore()
+            .collection('transactions')
+            .get()
+            .then(snapshot => {
+                snapshot.forEach(doc => {
+                    if(doc.data().owner == this.username)
+                        this.removeRow(doc.id)
+                });
+            })
+        },
+    getTimeStamp () {
+            return  new Date().getTime()
+        },
+  },
+  computed: {
+        balanceAvg () {
+            if (this.transactionList.length == 0){
+                return 0
+            }
+            else{
+                return this.totalBalance / this.transactionList.length
+            }
+        },
+        daysLeft () {
+            if (this.goal > this.totalBalance) {
+                return parseInt((this.goal - this.totalBalance) / this.balanceAvg)+1
+            }
+            else{
+                return 0
+            }
+        }
     }
-  }
 };
 </script>
 
